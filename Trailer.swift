7d68ac9a496a9659faf1991ec8d38f21f48da2ab
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-09-25 21:53:53 -0300

    Adicionando Trailers

---
+++ Trailer.swift
@@ -0,0 +1,8 @@
+//
+//  Trailer.swift
+//  TrailerFlix
+//
+//  Created by Eduardo Frederico on 25/09/24.
+//
+
+import Foundation
---
+++ TrailerFlix/Trailer.swift
@@ -0,0 +1,18 @@
+//
+//  Trailer.swift
+//  TrailerFlix
+//
+//  Created by Eduardo Frederico on 25/09/24.
+//
+
+import Foundation
+
+struct Trailer: Codable {
+    let title: String
+    let url: String
+    let rating: Int
+    let year: Int
+    let poster: String
+    
+    
+}
